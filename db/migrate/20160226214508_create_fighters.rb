768ff54c10bd631c491b386ce82dfad14c08fd02
Norbert <norbert.nytko@yahoo.com>
2016-02-27 01:50:01 +0100

    [Migration] Create fighters & skills

---
+++ db/migrate/20160226214508_create_fighters.rb
@@ -0,0 +1,11 @@
+class CreateFighters < ActiveRecord::Migration
+  def change
+    create_table :fighters do |t|
+      t.string :first_name
+      t.string :last_name
+      t.text :description
+
+      t.timestamps null: false
+    end
+  end
+end
