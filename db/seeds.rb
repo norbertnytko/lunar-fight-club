15d4f98bc67d5991b9926c06fc8fb7858d7a168f
Norbert <norbert.nytko@yahoo.com>
2016-03-01 00:29:50 +0100

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,59 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Seeding...'
+
+intelect = Skill.create!(name: 'Genius-Level Intellect', level: 5)
+martial_arts = Skill.create!(name: 'Martial Arts', level: 5)
+physical_condition = Skill.create!(name: 'Enhanced Physical Condition', level: 3)
+
+Fighter.create!(
+  first_name: 'Bruce',
+  last_name: 'Wayne',
+  description: "He's the hero Gotham deserves, but not the one it needs right now.",
+  skills: [
+    martial_arts,
+    intelect,
+    Skill.create!(name: "Kicking Superman's Ass", level: 5)
+    ]
+)
+
+Fighter.create!(
+  first_name: 'Joker',
+  description: 'Psychotic gunmen no longer seem to have any motive beyond a general anger',
+  skills: [
+    Skill.create!(name: 'Being Crazy', level: 5),
+    Skill.create!(name: 'Hurting realy, realy bad!', level: 1)
+  ]
+)
+
+Fighter.create!(
+  first_name: 'Clark',
+  last_name: 'Kent',
+  description: "It is a remarkable dichotomy. In many ways, Clark is the most human of us all. " \
+    "Then... he shoots fire from the skies and it is difficult not to think of him as a god. " \
+    "And how fortunate we all are that it does not occur to him",
+
+  skills: [
+    Skill.create!(name: 'Heat vision', level: 5),
+    Skill.create!(name: 'Flight', level: 2),
+    ]
+)
+
+Fighter.create!(
+  first_name: 'Slade',
+  last_name: 'Wilson',
+  description: "The world's greatest assassin.",
+  skills: [martial_arts, Skill.create!(name:'Fencing', level: 4) ]
+)
+
+Fighter.create!(
+  first_name: 'Tony',
+  last_name: 'Stark',
+  description: 'Genius billionaire playboy philanthropist.',
+  skills: [intelect, Skill.create!(name: 'Iron Man suit', level: 3)]
+)
+
+Fighter.create!(
+  first_name: 'Steven',
+  last_name: 'Rogers',
+  description: 'The first Avenger!',
+  skills: [physical_condition, martial_arts]
+)
