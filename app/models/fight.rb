8633f53138390fb3d3e5def1afbc78d0d57c5773
Norbert <norbert.nytko@yahoo.com>
2016-02-29 23:45:53 +0100

    Refactor Fight & Fighter models

--- app/models/fight.rb
+++ app/models/fight.rb
@@ -1,4 +1,14 @@
 class Fight < ActiveRecord::Base
-  has_many :fighter_fight
-  has_many :fighters, through: :fighter_fight
+  has_many :fighter_fights
+  has_many :fighters, through: :fighter_fights
+
+  belongs_to :winner, class_name: 'Fighter', foreign_key: :winner_id
+  belongs_to :losser, class_name: 'Fighter', foreign_key: :losser_id
+
+  after_create :settle_fight
+
+  private
+
+  def settle_fight
+  end
 end
--- app/models/fighter.rb
+++ app/models/fighter.rb
@@ -1,7 +1,11 @@
 class Fighter < ActiveRecord::Base
   has_many :skills, dependent: :destroy
-  has_many :fighter_fight
-  has_many :fights, through: :fighter_fight
+
+  has_many :fighter_fights
+  has_many :fights, through: :fighter_fights
+
+  has_many :wins, class_name: 'Fight', foreign_key: :winner_id
+  has_many :losses, class_name: 'Fight', foreign_key: :loser_id
 
   validates_presence_of :first_name, :description
   accepts_nested_attributes_for :skills
